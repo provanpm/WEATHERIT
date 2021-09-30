8a816def95b2d9d0da244f47ef7af04b23b4b85c
Newbtree45 <56883501+Newbtree45@users.noreply.github.com>
2021-09-30 19:48:15 -0400

    Add files via upload
    Initial spring boot application, controller, and index html files.

---
+++ WeatherIT/src/test/java/com/WeatherIT/WeatherItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeatherIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
