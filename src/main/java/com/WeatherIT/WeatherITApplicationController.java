d7f8a82232aefd7b394ad7d1b409a095a7491df9
provanpm <provanpm@mail.uc.edu>
2021-10-20 20:43:14 -0400

    Major Refactoring

---
+++ src/main/java/com/WeatherIT/WeatherITApplicationController.java
@@ -0,0 +1,13 @@
+package com.WeatherIT;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WeatherITApplicationController {
+	@RequestMapping("/index")
+	public String start() {
+		return "index";
+	}
+
+}
