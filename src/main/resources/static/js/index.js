88c91d209c26de5667e5d4811b9fa26e43977e32
provanpm <provanpm@mail.uc.edu>
2021-10-21 21:19:36 -0400

    OpenWeather API Working

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,6 +4,7 @@ const latitudeDisplay = document.getElementById(`latitude`);
 const longitudeDisplay = document.getElementById(`longitude`);
 const accuracyDisplay = document.getElementById(`accuracy`);
 const loadingStatus = document.getElementById(`loading`);
+const jsonDisplay = document.getElementById(`json`);
 
 let options = {
   enableHighAccuracy: true,
@@ -17,9 +18,21 @@ function success(pos) {
   latitudeDisplay.innerHTML = `<b>Latitude:</b> ${crd.latitude}`;
   longitudeDisplay.innerHTML = `<b>Longitude:</b> ${crd.longitude}`;
   accuracyDisplay.innerHTML = `Accuracy: within ${crd.accuracy} meters`;
+  
+  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&units=imperial&appid=8656dce640c0b11d88c31da21ed3c1fd`)
+  .then(function (response) {
+	response.json().then(function (data) {
+		jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit: ` + data.main.temp;
+	})
+  })
+  .catch(function (err) {
+	console.log(err);
+    jsonDisplay.innerHTML = `ERROR(${err.code}): ${err.message}`;
+  });
 }
 
 function error(err) {
+	console.log(err);
   loadingStatus.innerHTML = `ERROR(${err.code}): ${err.message}`;
 }
 
