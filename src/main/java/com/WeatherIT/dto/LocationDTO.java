1f78445c6c1e9f136d4651e91d5d30706f8d4c8a
Ben Miller <benm4643@gmail.com>
2021-10-31 22:06:52 -0400

    Create LocationDTO.java
    added LocationDTO so you could in the future save location for previous attempts and transition away from JS as this is a java focused class not javascript

---
+++ src/main/java/com/WeatherIT/dto/LocationDTO.java
@@ -0,0 +1,28 @@
+package com.WeatherIT.dto;
+
+public class LocationDTO {
+	
+	private float latitude;
+	private float longitude;
+	private float accuracy;
+	
+	public float getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(float latitude) {
+		this.latitude = latitude;
+	}
+	public float getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(float longitude) {
+		this.longitude = longitude;
+	}
+	public float getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(float accuracy) {
+		this.accuracy = accuracy;
+	}
+	
+}
