d7b68eb946241902309c866147f34501d1f05204
provanpm <provanpm@mail.uc.edu>
2021-11-17 12:42:49 -0500

    Using the One-Call to get and display weekly min max temp forecast

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -16,7 +16,19 @@ function success(pos) {
   fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${crd.latitude}&lon=${crd.longitude}&units=imperial&appid=8656dce640c0b11d88c31da21ed3c1fd`)
   .then(function (response) {
 	response.json().then(function (data) {
-		jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit: ` + data.current.temp;
+		jsonDisplay.innerHTML = ``;
+		for (let i = 0; i < 8; i++)
+		{
+            let milliseconds = data.daily[i].dt * 1000;
+
+            let dateObject = new Date(milliseconds)
+
+            let humanDateFormat = dateObject.toLocaleString()
+
+			jsonDisplay.innerHTML += humanDateFormat + ` Max Temperature at your location: ` + data.daily[i].temp.max + ` °F <br>`;
+			jsonDisplay.innerHTML += humanDateFormat + ` Min Temperature at your location: ` + data.daily[i].temp.min + ` °F <br>`;
+		    jsonDisplay.innerHTML += `<br>`;
+		}
 	})
   })
   .catch(function (err) {
@@ -32,7 +44,7 @@ function error(err) {
 
 refreshButton.addEventListener(`click`, function (event) {
   event.preventDefault();
-  jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit:`;
+  jsonDisplay.innerHTML = `Temperature at your location:`;
   loadingStatus.innerHTML = `Loading ...`;
   navigator.geolocation.getCurrentPosition(success, error, options);
 });
