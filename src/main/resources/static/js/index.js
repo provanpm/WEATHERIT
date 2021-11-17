c235c9ad5cc4835e80813ddf8c8913e5e1df1904
provanpm <provanpm@mail.uc.edu>
2021-11-17 12:13:56 -0500

    Changed to use One Call API for weather data

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,8 +1,5 @@
 // Elements
 const refreshButton = document.getElementById(`refresh-button`);
-const latitudeDisplay = document.getElementById(`latitude`);
-const longitudeDisplay = document.getElementById(`longitude`);
-const accuracyDisplay = document.getElementById(`accuracy`);
 const loadingStatus = document.getElementById(`loading`);
 const jsonDisplay = document.getElementById(`json`);
 
@@ -15,14 +12,11 @@ let options = {
 function success(pos) {
   loadingStatus.innerHTML = `Success!`;
   let crd = pos.coords;
-  latitudeDisplay.innerHTML = `<b>Latitude:</b> ${crd.latitude}`;
-  longitudeDisplay.innerHTML = `<b>Longitude:</b> ${crd.longitude}`;
-  accuracyDisplay.innerHTML = `Accuracy: within ${crd.accuracy} meters`;
-  
-  fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&units=imperial&appid=8656dce640c0b11d88c31da21ed3c1fd`)
+
+  fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${crd.latitude}&lon=${crd.longitude}&units=imperial&appid=8656dce640c0b11d88c31da21ed3c1fd`)
   .then(function (response) {
 	response.json().then(function (data) {
-		jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit: ` + data.main.temp;
+		jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit: ` + data.current.temp;
 	})
   })
   .catch(function (err) {
@@ -38,9 +32,6 @@ function error(err) {
 
 refreshButton.addEventListener(`click`, function (event) {
   event.preventDefault();
-  latitudeDisplay.innerHTML = `<b>Latitude:</b>`;
-  longitudeDisplay.innerHTML = `<b>Longitude:</b>`;
-  accuracyDisplay.innerHTML = `Accuracy:`;
   jsonDisplay.innerHTML = `Temperature at your location in degrees Fahrenheit:`;
   loadingStatus.innerHTML = `Loading ...`;
   navigator.geolocation.getCurrentPosition(success, error, options);
