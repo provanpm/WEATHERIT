443b4d2ede10a041d8aa0676fd262389f51b6bd6
provanpm <provanpm@mail.uc.edu>
2021-11-18 20:43:10 -0500

    Days of week + more formatting

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -7,6 +7,14 @@ const locationDisplay = document.getElementById(`location`);
 const key = `8656dce640c0b11d88c31da21ed3c1fd`;
 const apiWeatherLocation = `https://api.openweathermap.org/data/2.5/weather?`;
 const apiOneCallForecast = `https://api.openweathermap.org/data/2.5/onecall?`;
+const weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
 let options = {
   enableHighAccuracy: true,
@@ -48,15 +56,17 @@ function loadForecast(crd) {
 		{
             let milliseconds = data.daily[i].dt * 1000;
             let dateObject = new Date(milliseconds);
-			let month = dateObject.getMonth();
+			let month = dateObject.getMonth() + 1;
 			let day = dateObject.getDate();
 			let year = dateObject.getFullYear();
+			let DayOfWeek = weekday[dateObject.getDay()];
             let humanDateFormat = month + '/' + day + '/' + year;
 
+			let dayParagraph = `<p>${DayOfWeek}</p>`
 			let dateParagraph = `<p>${humanDateFormat}</p>`;
-			let maxParagraph = `<p>Max Temp: ${data.daily[i].temp.max} °F</p>`;
-			let minParagraph = `<p>Min Temp: ${data.daily[i].temp.min} °F</p>`;
-			let newForecastItem = dateParagraph + maxParagraph + minParagraph;
+			let maxParagraph = `<p>Max Temp:<br>${data.daily[i].temp.max} °F</p>`;
+			let minParagraph = `<p>Min Temp:<br>${data.daily[i].temp.min} °F</p>`;
+			let newForecastItem = dayParagraph + dateParagraph + maxParagraph + minParagraph;
 
 			forecastDisplay.innerHTML += `<li class="list-group-item">${newForecastItem}</li>`;
 		}
